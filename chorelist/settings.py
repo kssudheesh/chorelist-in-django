9af410c39d2068949fb99118ff996b6cd8071251
kssudheesh <kssudheesh001@gmail.com>
2016-02-24 22:07:13 +0530

    reverting back to original

--- chorelist/settings.py
+++ chorelist/settings.py
@@ -82,10 +82,6 @@ DATABASES = {
     }
 }
 
-# Update database configuration with $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 
 # Internationalization
@@ -105,22 +101,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-
-
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
--- chorelist/wsgi.py
+++ chorelist/wsgi.py
@@ -7,21 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-#import os
-
-#from django.core.wsgi import get_wsgi_application
-
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chorelist.settings")
-
-#application = get_wsgi_application()
-
-
-
+import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chorelist.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+
+
+
 
 
